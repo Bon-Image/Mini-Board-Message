2175200abfebd2fd2b4607a269248fe4ef335840
Bon-Image <bonimage@pm.me>
2025-03-26 07:46:19 +0100

    setp the form and configured the newMessageRouter to handle the form

--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -3,7 +3,23 @@ import { Router } from 'express';
 const newMessageRouter = Router(); 
 
 newMessageRouter.get('/', (req, res) => {
-    res.send("It also works, you are in the new messages page")
+    res.render('form'); 
 }); 
+
+
+newMessageRouter.post('/', (req, res) => {
+    const { user, text } = req.body;
+  
+    if (user && text) {
+      messages.push({
+        user,
+        text,
+        added: new Date()
+      });
+    }
+  
+    res.redirect('/');
+  });
+
 
 export default newMessageRouter; 
